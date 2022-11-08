2eace1fec1b041b67d9b44b4313efeb930aa7af0
sbhojne1990 <sbhojne1990@gmail.com>
2022-11-08 17:31:12 +0530

    added

--- 0611.py
+++ 0611.py
@@ -1,34 +1,35 @@
-# listA =[1989,1990,1991,1992]
-# listB =[33,32,31,30]
-# for items in listA:
-#     # print(2022-items)
-#     listB.append(2022-items)
-# print(listB)
+listA =[1989,1990,1991,1992]
+listB =[33,32,31,30]
+for items in listA:
+    # print(2022-items)
+    listB.append(2022-items)
+print(listB)
 
-# Map
-# q2=list(map(lambda x:2022-x,listA))
-# print(q2)
+#Map
+q2=list(map(lambda x:2022-x,listA))
+print(q2)
 
 
-# listC=[11,1,2,33,4,4,55]
+listC=[11,1,2,33,4,4,55]
 
-# q3=list(map(lambda x:x*3,listC))
-# print(q3)
+q3=list(map(lambda x:x*3,listC))
+print(q3)
 
-# listD=[11,22,33,44,55,66]
-# q4 = list(map(lambda y:y*3,listD))
-# print(q4)
+listD=[11,22,33,44,55,66]
+q4 = list(map(lambda y:y*3,listD))
+print(q4)
 
-# listE = [11,22,33,44,55,66,77]
-# listf=[]
+listE = [11,22,33,44,55,66,77]
+listf=[]
 
-# for items in listE:
-#     if items>50:
-#         listf.append(items)
-# print(listf)
+for items in listE:
+    if items>50:
+        listf.append(items)
+print(listf)
         
-# q5 = list(filter(lambda x:x>50,listE))
-# print(q5)
+q5 = list(filter(lambda x:x>50,listE))
+print(q5)
 
 listG = [33,44,55,22,3,477,88,99]
 print(list(filter(lambda x : x%2 == 0,listG)))
+
